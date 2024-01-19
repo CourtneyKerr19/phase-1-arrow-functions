7c4fc6391583e7d40bc0430541952ee07627995d
Courtney Kerr <courtneykerr219@gmail.com>
2024-01-18 18:00:01 -0600

    complete

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = (parameter1, parameter2) => parameter1/ parameter2;
+
+const square = (num) => {
+    return num ** 2;
+}; 
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
